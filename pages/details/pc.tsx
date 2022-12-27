d025b196a48d28a7d3f4be0a7dafd639935f6d40
Ashwin <69374625+xxiz@users.noreply.github.com>
2022-12-26 18:25:11 -0800

    embed to iframe for safari support

--- pages/details/pc.tsx
+++ pages/details/pc.tsx
@@ -9,7 +9,7 @@ function PC() {
         <Layout>
             <Container>
             <div className="max-w-screen-xl px-6 mx-auto mt-10">
-            <embed type="application/pdf" src="/static/documents/portello.pdf" className="mx-auto w-full md:w-[700px] h-screen" />
+            <iframe src="/static/documents/portello.pdf" className="mx-auto w-full md:w-[700px] h-screen" />
           </div>
             </Container>
         </Layout>        
