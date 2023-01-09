c8c430b1a396917530a2f99b8377be736bd9abc4
Ashwin <69374625+xxiz@users.noreply.github.com>
2023-01-09 11:34:38 -0800

    fix: analytics

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,6 +6,8 @@ function Layout({ children, title }: any) {
     <Head>
       <title>Ashwin — {title}</title>
       <link rel="icon" type="image/x-icon" href="https://safe.1m.cx/Z6G18xQ7.png" />
+      
+    <script async defer data-website-id="d4f97cd3-807b-4837-964d-3b6e7525f991" src="https://umami.1m.cx/umami.js" />
     </Head>
     <div className="flex flex-col min-h-screen antialiased bg-zinc-900 text-zinc-400">
       {children}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
   <>
-  <Script async defer data-website-id="d4f97cd3-807b-4837-964d-3b6e7525f991" src="https://umami.1m.cx/umami.js" />
   <Component {...pageProps} />
   </>)
 }
