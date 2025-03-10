914563f6ed3e2c7855470f87242ce696cbb41ffd
Ashwin <69374625+xxiz@users.noreply.github.com>
2025-03-10 14:42:48 -0700

    refactor: add horizontal padding to BlogPostLayout for improved responsiveness

--- src/components/Blog/PostLayout.tsx
+++ src/components/Blog/PostLayout.tsx
@@ -4,7 +4,7 @@ export default function BlogPostLayout({
     children: React.ReactNode;
   }) {
     return (
-      <div className="w-full max-w-4xl mx-auto py-12">
+      <div className="w-full max-w-4xl mx-auto py-12 px-4 md:px-0">
         {children}
       </div>
     );
