397ab5d0bef9a1e1728ed25da6c2dd76f4ae41ab
Ashwin <69374625+xxiz@users.noreply.github.com>
2025-09-08 14:50:10 -0700

    feat: add new project entry for SIEM system and associated image

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,4 +1,12 @@
 const projects = [
+
+  {
+    title: "Management System",
+    description: "A management system for a local club, built with GoLang, GraphQL, and PostgreSQL.",
+    image: "/projects/baulkham.png",
+    tags: ["GoLang", "GraphQL", "PostgreSQL"],
+    link: "https://baulkhamhillsvolleyball.com.au/",
+  },
   {
     title: "Personal Website",
     description: "My personal website built with Next.js, TypeScript, and Tailwind CSS. Features a blog, projects showcase, and more.",
@@ -12,15 +20,7 @@ const projects = [
     description: "A full-stack website that alerts a user when specific deals are on RedFlagDeals.com",
     image: "/projects/rfd.png",
     tags: ["Express", "React", "MongoDB"],
-    link: "#",
     github: "/socials/github/SalesScout"
-  },
-  {
-    title: "Management System",
-    description: "A management system for a local club, built with GoLang, GraphQL, and PostgreSQL.",
-    image: "/projects/baulkham.png",
-    tags: ["GoLang", "GraphQL", "PostgreSQL"],
-    link: "https://baulkhamhillsvolleyball.com.au/",
   },
   {
     title: "Digispark",
@@ -28,6 +28,13 @@ const projects = [
     image: "/projects/digispark.jpg",
     tags: ["Arduino", "Digispark"],
     github: "/socials/github/Digispark"
+  },
+  {
+    title: "SIEM (In Progress)",
+    description: "Build a SIEM system to monitor and analyze security events in real-time.",
+    image: "/projects/siem.png",
+    tags: ["Security", "Management"],
+    github: "/socials/github/it-geeks"
   }
 ];
 
